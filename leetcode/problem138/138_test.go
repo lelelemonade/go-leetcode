681860ebf58fa464be81134e0d131b94b0733e6a
zhonglishen <szl921818@gmail.com>
2024-08-18 12:34:43 +0200

    feat:138

--- leetcode/problem138/138_test.go
+++ leetcode/problem138/138_test.go
@@ -1,13 +1,42 @@
 package problem138
 
 type Node struct {
-    Val int
-    Next *Node
-    Random *Node
+	Val    int
+	Next   *Node
+	Random *Node
 }
 
+func copyRandomList(head *Node) *Node {
+	oldNodeToIdxMap := make(map[*Node]int)
+	newIdxToNodeMap := make(map[int]*Node)
 
- func copyRandomList(head *Node) *Node {
-    nextMap:=make(map[*Node]*Node)
-	nextMap:=make(map[*Node]*Node)
+	newDummyHead := &Node{}
+	newIter := newDummyHead
+
+	oldIter := head
+	idx := 0
+	for oldIter != nil {
+		newTemp := &Node{
+			Val: oldIter.Val,
+		}
+		newIter.Next = newTemp
+		newIter = newTemp
+
+		oldNodeToIdxMap[oldIter] = idx
+		newIdxToNodeMap[idx] = newIter
+		oldIter = oldIter.Next
+        idx++
+	}
+
+	oldIter = head
+	newIter = newDummyHead.Next
+	for oldIter != nil {
+        if oldIter.Random != nil {
+            newIter.Random = newIdxToNodeMap[oldNodeToIdxMap[oldIter.Random]]
+        }
+		oldIter = oldIter.Next
+        newIter=newIter.Next
+	}
+
+	return newDummyHead.Next
 }
